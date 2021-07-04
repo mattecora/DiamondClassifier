073b3082fe7f3948ca62eb2d6a14f31801793d61
Copsha <gabrielecoppola96@gmail.com>
2021-07-04 18:05:09 +0000

    Modified Model.py and other adjustments

--- sagemaker/Model.py
+++ sagemaker/Model.py
@@ -14,17 +14,11 @@ def model_fn(model_dir):
     return clf
 
 def input_fn(request_body, request_content_type):
-    """An input_fn that loads a pickled numpy array"""
     if request_content_type == "text/csv":
-        array = np.fromstring(request_body, sep = ',')
+        array = np.array([[float(value) for value in line.split(",")] for line in request_body.split("\n")])
         return array
     else:
-        raise ValueError('Only text/csv and application/json are supported')
-    """
-    elif request_content_type == "application/json":
-        array = np.load(StringIO(request_body))
-        return array 
-    """
+        raise ValueError('Only text/csv format is supported')
 
 def predict_fn(input_data, model):
     prediction = model.predict(input_data)
@@ -34,37 +28,30 @@ def output_fn(prediction, content_type):
     if content_type == "text/csv":
         return ",".join([str(x) for x in prediction])
     else:
-        raise ValueError('Only text/csv and application/json are supported')
-
-
+        raise ValueError('Only text/csv format is supported')
 
 if __name__ =='__main__':
-
     parser = argparse.ArgumentParser()
     
-    # Data, model, and output directories
-    #parser.add_argument('--output-data-dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR'))
     parser.add_argument('--model-dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
     parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
     
     args, _ = parser.parse_known_args()
     
-    train = pd.read_csv(os.path.join(args.train, "prep_train.csv"), engine='python')
-    X_train = train.drop(labels = ['carat_class'],axis = 1)
+    train = pd.read_csv(os.path.join(args.train, "prep_train.csv"), engine = 'python')
+    X_train = train.drop(labels = ['carat_class'], axis = 1)
     y_train = train['carat_class']
     
     seed = 42
 
     std_scale = StandardScaler()
-    pca = PCA(n_components=5)
-    clf = SVC(gamma='auto',kernel='rbf', C = 1.0, random_state = seed)
+    pca = PCA(n_components = 5)
+    clf = SVC(gamma = 'auto', random_state = seed)
     
     parameters = {'kernel':('linear', 'rbf'), 'C':[0.01, 0.1, 1, 10, 100]}
     grid = GridSearchCV(clf, parameters)
     
-    model = make_pipeline(std_scale,pca,grid)
-
-    #sample_train, sample_val, label_train, label_val
+    model = make_pipeline(std_scale, pca, grid)
     model.fit(X_train, y_train)  
     
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
